ce0c66fee4a07caf3a4f6ce82281e5a3961eeafc
wsyxbcl <wsyxbcl@gmail.com>
2020-07-08 01:54:37 +0800

    basic function defined

--- pyexe/structure_enumerate.py
+++ pyexe/structure_enumerate.py
@@ -2,7 +2,20 @@ import ase.io
 from ase.db import connect
 from icet.tools import enumerate_structures
 
-def select_structures():
+def select_structures(database, remove_x=True):
+    """
+    select structures from enumerated structure database for calculation
 
-def structures_to_vasp():
+    database: ase database
+    remove_x: remove X element from structure where X represents vacancy
+    """
 
+def structures_to_vasp(structures, path_calc):
+    """
+    generate POSCAR and set up directorys for VASP calculaiton
+    """
+
+def vasp_to_database(database, path_calc, property):
+    """
+    extract calculated properties and add back to database
+    """
