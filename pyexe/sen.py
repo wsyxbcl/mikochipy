e266086e7408cb2e25db2bc2c1b6cdce37378256
wsyxbcl <wsyxbcl@gmail.com>
2020-07-09 05:08:53 +0800

    add logging

--- pyexe/sen.py
+++ pyexe/sen.py
@@ -7,6 +7,7 @@
 # designed specificly for batch job submission for constructed CE dataset
 # Will be reconstructed #TODO
 
+import logging
 import os
 from pathlib import Path
 import re
@@ -54,17 +55,27 @@ def get_bjobs_status():
     return bjobs_status
 
 def auto_vasp_ce(dataset_dir, max_job=6, time_sleep=1000):
+    logging.basicConfig(
+        filename='auto_vasp_ce.log',
+        level=logging.DEBUG,
+        format='%(levelname)s:%(asctime)s: %(message)s'
+    )
+
     # Specificly for batch job submission for constructed CE dataset
     print("Job begins")
+    logging.debug('Job begins')
     print("Dataset: "+str(dataset_dir))
+    logging.info("Dataset: "+str(dataset_dir))
     task_dirs = [rootdir.joinpath(rootdir, name) for name in os.listdir(rootdir)
                  if os.path.isdir(os.path.join(rootdir, name))]
     print("Total task: {}".format(len(task_dirs)))
+    logging.info("Total task: {}".format(len(task_dirs)))
     # Remove fininshed tasks 
     for task_dir in task_dirs:
         if os.path.exists(task_dir.joinpath('OUTCAR')):
             task_dirs.remove(task_dir)
     print("Task left: {}".format(len(task_dirs)))
+    logging.info("Task left: {}".format(len(task_dirs)))
     print("\nGood luck by Sen")
     
     while True:
@@ -73,13 +84,16 @@ def auto_vasp_ce(dataset_dir, max_job=6, time_sleep=1000):
             task_dir = random.choice(task_dirs)
             job = Bjob(task_dir=task_dir, type='vasp')
             print("Job created from: {}".format(str(task_dir)))
+            logging.info("Job created from: {}".format(str(task_dir)))
             job.submit()
             print("Job submmited as {}, status: {}".format(job.jobid, job.get_stat()))
+            logging.info("Job submmited as {}, status: {}".format(job.jobid, job.get_stat()))
             task_dirs.remove(task_dir)
             print("Task removed.")
             continue
         else:
             print("zzzzzzz")
+            logging.debug("sleep")
             time.sleep(time_sleep)
 
 if __name__ == '__main__':
