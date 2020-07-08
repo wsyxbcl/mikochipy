4280e608a16a2ed15e802faaa75f667704143391
wsyxbcl <wsyxbcl@gmail.com>
2020-07-09 03:14:59 +0800

    trail version of Bjob for testing

---
+++ pyexe/sen.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+# Automation job submission.
+# Named after Sen.
+
+# Currently focus on Platform Load Sharing Facility (LSF)
+# designed for batch job submission for construct CE dataset
+
+import re
+from pathlib import Path
+import subprocess 
+
+class Bjob():
+    def __init__(self, task_dir, type):
+        self.task_dir = task_dir
+        self.type = type
+        self.status = None
+        self.jobid = None
+
+    def get_stat(self):
+        if self.jobid is None:
+            self.status = 'TODO'
+        else:
+            status = subprocess.check_output(['bjobs', self.jobid])
+            # TODO
+            # Just a temporary implement
+            stat = status.decode('utf-8').split()[10]
+            self.stat = stat
+
+    def submit(self):
+        if self.type == 'vasp':
+            vasp_file = self.task_dir.joinpath('vasp.lsf')
+            job = subprocess.check_output('bsub < '+str(vasp_file), shell=True)
+            pipe = subprocess.Popen(['bsub', '-L', '/bin/sh'], 
+                                     stdout=subprocess.PIPE, 
+                                     stderr=subprocess.PIPE, 
+                                     stdin=open(vasp_file, 'r'))
+            job = pipe.communicate()[0]
+            self.jobid = re.findall('\<(.*?)\>', job.decode('utf-8'))[0]
+
+
+
