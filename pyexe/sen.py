ec5f62fc7737cccee21c8474880e26a982566bff
wsyxbcl <wsyxbcl@gmail.com>
2020-07-09 03:30:04 +0800

    fallback to check_output in submit

--- pyexe/sen.py
+++ pyexe/sen.py
@@ -34,10 +34,10 @@ class Bjob():
         if self.type == 'vasp':
             vasp_file = self.task_dir.joinpath('vasp.lsf')
             job = subprocess.check_output('bsub < '+str(vasp_file), shell=True)
-            pipe = subprocess.Popen(['bsub', '-L', '/bin/sh'], 
-                                     stdout=subprocess.PIPE, 
-                                     stderr=subprocess.PIPE, 
-                                     stdin=open(vasp_file, 'r'))
+            # pipe = subprocess.Popen(['bsub', '-L', '/bin/sh'], 
+            #                          stdout=subprocess.PIPE, 
+            #                          stderr=subprocess.PIPE, 
+            #                          stdin=open(vasp_file, 'r'))
             job = pipe.communicate()[0]
             self.jobid = re.findall('\<(.*?)\>', job.decode('utf-8'))[0]
 
