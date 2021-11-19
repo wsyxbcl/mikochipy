4a0a74c1cc853d5afbf4e464bc48ad875ec6685f
wsyxbcl <wsyxbcl@gmail.com>
2021-11-19 21:18:14 +0800

    chemdoc

---
+++ pyexe/chemdoc.py
@@ -0,0 +1,30 @@
+# Parsing chemical formulas in doc files
+# Working in an extremely low-efficient approach (just like Microsoft Word itself)
+
+from docx import Document
+from pyvalem.formula import Formula, FormulaParseError
+
+def main():
+    input_file_path = 'test.docx'
+    output_file_path = 'result.docx'
+
+    doc = Document(input_file_path)
+
+    # Here comes the nesting loops!
+    for paragraph in doc.paragraphs:
+        for word in paragraph.text.split():
+            try:
+                replace_text_in_paragraph(paragraph, word, Formula(word).html)
+            except FormulaParseError:
+                continue
+
+    doc.save(output_file_path)
+
+def replace_text_in_paragraph(paragraph, key, value):
+    inline = paragraph.runs
+    for item in inline:
+        if key in item.text:
+            item.text = item.text.replace(key, value)
+
+if __name__ == '__main__':
+    main()
